3f622a46b5dd761f13f19eab7d475180d5e21443
MattC on Ubuntu <dechtmatt@gmail.com>
2013-03-13 15:01:31 -1000

    Added more detailed usage comments to segregate.pl

--- segregate.pl
+++ segregate.pl
@@ -3,9 +3,24 @@
 #given the filenames of one or more sequence alignment files, this 
 #script will output a similar file, with sequences marked by the 
 #same identifier adjacent to each other, and sorted by the identifiers.
+#Note: the first white space on each line defines the separation between identifier
+#      and data fields. 
+#      Lines must be terminated by an end-of-line character.
 
-#lines in the input file may look something like:
-#cp222993_c0_s1  CAAGATGACAATAAAGAGGAAGTAAGCCAGACCGAGGTGTGGGATCAGGGGGAGCCCGTG
+#lines in the input file(s) may look something like:
+#cp222993_c0_s1 --------------------------------------DDSRVGSPNGSLD------GGV
+#cp44060_c3_s1 RREKYKKQEDEREVVRQSIREKYGLEKPPNDFSDDDDEDDDCDKGDDGRGGSEMQSRSGL
+#
+#cp222993_c0_s1 -----------------------------------------IQLGQG-------------
+#cp44060_c3_s1 PSPGRESTTFTNKYPSRTLPANSYEQVDKSEDPETTTDRMSI---RSATLSLYKSEESLD
+
+#lines in the corresponding output file will be:
+#cp222993_c0_s1 --------------------------------------DDSRVGSPNGSLD------GGV
+#cp222993_c0_s1 -----------------------------------------IQLGQG-------------
+#
+#cp44060_c3_s1 RREKYKKQEDEREVVRQSIREKYGLEKPPNDFSDDDDEDDDCDKGDDGRGGSEMQSRSGL
+#cp44060_c3_s1 PSPGRESTTFTNKYPSRTLPANSYEQVDKSEDPETTTDRMSI---RSATLSLYKSEESLD
+
 
 #example usage: 
 #perl segregate.pl file1.txt file2.txt >alldata.txt
