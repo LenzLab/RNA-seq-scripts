73d77c8bf0c384e0ea52b9d0228b40d2e4cb9fb7
MattC on Ubuntu <dechtmatt@gmail.com>
2013-06-21 14:45:54 -1000

    Added segmentFinder.pl A script for detecting simple segment breaks in MAFFT alignement files. See scriptdescriptions.txt for more detail.

---
+++ segmentFinder.pl
@@ -0,0 +1,232 @@
+#segmentFinder.pl   MC   14-6-2013
+
+#usage: perl segmentFinderV1.pl MAFFTfile.txt
+
+#Reads a MAFFT alignment file and detects segments.
+#
+#detects a segment change when the pattern of differences
+#between two columns changes, or if a strand changes from blank (hyphens) to coding
+#If there are consecutive segments shorter than $MIN_SEGMENT_LENGTH 
+#(this value can be set below; set to 1 or less for no segment combination), 
+#combines them into a composite segment. 
+#Output is a list of segment lengths and multiplicities (for non-composites)
+#in the order that they appear.
+
+#a copy of the MAFFT file with the beginning of each segment marked with its length is also printed.
+#be aware that a segment length of 1 may be misleading, as there will be no gap between its marker
+#and the length of the next segment. For example, a 1-segment followed by an 18-segment will appear
+#to be marked as a 118-segment.
+
+#!usr/bin/per
+use strict;
+
+my $MIN_SEGMENT_LENGTH = 10;
+
+
+
+
+
+my $MAFFTfile = $ARGV[0];
+
+my @lineBuffer;
+my @seqBuffer;
+my $currentPos = 0;
+my $segLength = 0;
+my $segNumber = 0;
+my @thisPattern;
+my @lastPattern;
+my $thisMulti;
+my $lastMulti;
+my $bufferReady = 0;
+my @segments;
+
+
+open(my $fh, $MAFFTfile) or die $!;
+
+#main loop for segment detecting
+SECTION: while(1){
+
+	#if between sections, load the next section as lines into an array
+	while(!$bufferReady){
+		$_ = <$fh>;
+		if(!$_){
+			if(@lineBuffer == 0){
+				last SECTION;
+			}else{
+				$bufferReady = 1;
+			}
+		}elsif(!/^(\S+?)\s+(\S+)\s*$/){
+			if(@lineBuffer == 0){
+				next;
+			}else{
+				$bufferReady = 1;
+			}
+		}else{
+			push(@lineBuffer, $_);	
+		}
+	}
+
+	#load the sequencing part of each line individually into another array, 
+	#each as an array of individual characters
+	foreach my $line (@lineBuffer){
+		$line =~ /^(\S+?)\s+(\S+)\s*$/;
+		my @lineArray = split("", $2);
+		push(@seqBuffer, \@lineArray);
+	}
+
+	#shift one column of a single character over and compare for a 
+	#possible segment change
+	while(@{$seqBuffer[0]}){
+		@lastPattern = @thisPattern;
+		undef(@thisPattern);
+		
+		my %patternHash;
+		$lastMulti = $thisMulti;
+		$thisMulti = 0;
+		#determine the pattern for this column
+		for(my $i = 0; $i < @seqBuffer; $i++){
+			my $char = shift(@{$seqBuffer[$i]});
+			if($char eq "-"){
+				push(@thisPattern, "-");
+				$thisMulti = keys(%patternHash) + 1;
+			}elsif(exists($patternHash{$char})){
+				push(@thisPattern, $patternHash{$char});
+			}else{
+				my $numSeqs = keys(%patternHash);
+				$patternHash{$char} = $numSeqs + 1;
+				push(@thisPattern, $patternHash{$char});
+				$thisMulti++;
+			}
+		}
+		if(@lastPattern){
+			#compares the patterns of the current column
+			#with the previous column to detect a segment 
+			#boundary
+			for(my $i = 0; $i < @lastPattern; $i++){
+				if( $lastPattern[$i] ne $thisPattern[$i] ){
+					$segNumber++;
+					push(@segments, {
+							   "length" => $segLength,
+							   "multiplicity" => $lastMulti,
+							   "isComposite" => 0
+							}
+					);
+					$segLength = 0;
+					last;
+				}
+			}
+		}
+		$segLength++;
+		$currentPos++;
+	}
+
+	$bufferReady = 0;
+	undef(@lineBuffer);
+	undef(@seqBuffer);
+}
+close($fh);
+#enter final segment since the loop only reports segments when it finds a new one
+$segNumber++;
+push(@segments, {
+		   "length" => $segLength,
+		   "multiplicity" => $lastMulti,
+		   "isComposite" => 0
+		}
+);
+
+#find consecutive short segments and combine them into composite segments
+for(my $i = 1; $i < @segments; $i++){
+	my %thisSegment = %{$segments[$i]};
+	if($thisSegment{"length"} < $MIN_SEGMENT_LENGTH){
+		my %lastSegment = %{$segments[$i-1]};
+		if($lastSegment{"isComposite"} or
+		   $lastSegment{"length"} < $MIN_SEGMENT_LENGTH
+		  ){
+			$lastSegment{"isComposite"} = 1;
+			$lastSegment{"length"} += $thisSegment{"length"};
+			$segments[$i-1] = \%lastSegment;
+			splice(@segments, $i, 1);
+			$i--;
+		}
+	}
+}
+
+#segment info output by columns
+print "\$MIN_SEGMENT_LENGTH = $MIN_SEGMENT_LENGTH\n";
+print("Seg #\tLength\tMultiplicity\n");
+for(my $i = 1; $i <= @segments; $i++){
+	my %segment = %{$segments[$i-1]};
+	print $i."\t".$segment{"length"};
+	if(!$segment{"isComposite"}){
+		print "\t".$segment{"multiplicity"};
+	}
+	print "\n";
+}
+print "\n";
+
+#output MAFFT file with segments marked
+my $outText = "";
+my $indentLength;
+my $sectionLength;
+my $lineLength;
+my $segIndex = 0;
+my $markerLine;
+my $spacesLeft = 0;
+my $currentSection;
+
+open($fh, $MAFFTfile) or die $!;
+while(){
+	if(!$currentSection){
+		my $haveLines = 0;
+		while(<$fh>){
+			if(/^(\S+\s+)(\S+)\s*$/){
+				$currentSection .= $_;
+				$haveLines = 1;
+	
+				if(!$lineLength){
+					$indentLength = length($1);
+					$sectionLength = length($2);
+					$lineLength = $indentLength + $sectionLength;
+					$markerLine = " " x $indentLength;
+				}
+			}elsif(!/\S/ and $haveLines){
+				$currentSection .= $_;
+				last;
+			}
+		}
+	}
+
+	if(length($markerLine) >= $lineLength){
+		$outText .= $markerLine . "\n";
+		$markerLine = " " x $indentLength;
+		$outText .= $currentSection;
+		$currentSection = "";
+
+	}elsif($spacesLeft == 0){
+		if($segIndex < @segments){
+			my %segment = %{$segments[$segIndex]};
+			$segIndex++;
+			my $segLength = $segment{"length"};
+			$spacesLeft = $segLength;
+	
+			my $charsLeftOnLine = $lineLength - length($markerLine);
+			if($charsLeftOnLine < length($segLength)){
+				$spacesLeft -= $charsLeftOnLine;
+			}else{
+				$spacesLeft -= length($segLength);
+			}
+			$markerLine .= $segLength;
+		}else{
+			$outText .= $markerLine . "\n";
+			$outText .= $currentSection;
+			$currentSection = "";
+			last;
+		}
+	}else{
+		$markerLine .= " ";
+		$spacesLeft--;
+	}
+}
+close($fh);
+
+print $outText;
