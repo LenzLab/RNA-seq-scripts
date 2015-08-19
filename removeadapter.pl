58dde52b655a1d7b47ed1ee436ae99bef8756c07
Julian Hartline <julianh2o@gmail.com>
2015-08-19 01:44:40 +0100

    added new script to remove adapters from a fasta file

---
+++ removeadapter.pl
@@ -0,0 +1,55 @@
+#removeadapter.pl - 2015-8-18 jh
+
+
+#Usage: perl countconsec.pl filename.fasta adapters.txt
+#       i.e.:  perl countconsec.pl small.fasta
+#              perl countconsec.pl small.fasta 7
+# This script takes a fasta file and an adapter file and removes
+# the adapter sequences from each fasta sequence
+# it outputs a new fasta file with the adapter sequences removed
+
+#!usr/bin/perl
+
+use Fasta;
+use strict;
+
+my $fasta = new Fasta($ARGV[0]) or die "need a fasta file\n";
+my $adapter =  $ARGV[1] or die "need an adapter file\n";
+my $lineLength = 70;
+
+my @adapterSequences;
+open(FH,$adapter) || die "Cannot open $adapter";
+while (my $line = <FH>) {
+    chomp $line;
+	if ($line =~ /^5.* (.*)$/) {
+		#print("adding: ".$1."\n");
+		push(@adapterSequences,$1);
+	}
+}
+
+my $out = "";
+while($fasta->hasNext()){
+	my $entry = $fasta->next();
+	my $ident = $entry->{identifier};
+	my $header = $entry->{header};
+	my $transcript = $entry->{transcript};
+	
+	#print ("seesq: @adapterSequences\n");
+	#print ("start length: ".length($transcript)."\n");
+	foreach(@adapterSequences) {
+		$adapter = $_;
+		#print("removing ".$adapter);
+		$transcript =~ s/$adapter//g;
+		#print("  ".length($transcript)."\n");
+	}
+
+	$out .= "$header";
+	while(length($transcript) > $lineLength) {
+		$out .= substr($transcript,0,$lineLength)."\n";
+		$transcript = substr($transcript,$lineLength);
+	}
+	$out .= $transcript."\n";
+	$out .= "\n";
+}
+
+print $out;
